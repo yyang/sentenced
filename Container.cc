1a34b4e8b31d795db770274e88a775e5eae3bf3b
root <root@OpenStackNode1.iasi-ryc.intel.com>
2011-08-29 14:56:47 +0800

    some modification in Container.cc

--- Container.cc
+++ Container.cc
@@ -33,9 +33,8 @@ bool Container::load(const char* file_path)
 	while(getline(ifs, temp)&&temp.length()!=0)
 	{
 		//To split out the space character
-			cout<<temp.length()<<", "<<temp<<endl;
+		//		cout<<temp.length()<<", "<<temp<<endl;
 		dict->insert(temp.substr(0, temp.length()));
-
 	}
 	//cout<<dict->size()<<endl;
 }
@@ -51,14 +50,14 @@ bool Container::include(const string & des)
 		return false;
 }
 /*
-int main()
-{
-	Container con;
-	con.load("symbols.txt");
-	string temp;
-	cout<<con.include(string("0"))<<endl;
-	cout<<con.include(string("1"))<<endl;
-	cout<<con.include("+")<<endl;
-	cout<<con.include("^")<<endl;
-	con.release();
-}*/
+   int main()
+   {
+   Container con;
+   con.load("symbols.txt");
+   string temp;
+   cout<<con.include(string("0"))<<endl;
+   cout<<con.include(string("1"))<<endl;
+   cout<<con.include("+")<<endl;
+   cout<<con.include("^")<<endl;
+   con.release();
+   }*/
